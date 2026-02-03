05851f2b1e009aae30cd8e6027e384f0da731342
Mahmoud Rafat <mahmoudraft2002@gmail.com>
2026-02-03 20:47:58 +0000

    Edit output.tf

--- modules/apigateway/output.tf
+++ modules/apigateway/output.tf
@@ -1,16 +1,16 @@
 output "invoke_url" {
   description = "API invoke URL"
-  value       = aws_apigatewayv2_stage.this.invoke_url
+  value       = aws_apigatewayv2_stage.NTI_STAGES.invoke_url
 }
 
 output "api_id" {
   description = "HTTP API ID"
-  value       = aws_apigatewayv2_api.this.id
+  value       = aws_apigatewayv2_api.NTI_API.id
 }
 
 output "vpclink_id" {
   description = "VPC Link ID"
-  value       = aws_apigatewayv2_vpc_link.this.id
+  value       = aws_apigatewayv2_vpc_link.NTI_VPC_LINK.id
 }
 
 output "nlb_arn" {
